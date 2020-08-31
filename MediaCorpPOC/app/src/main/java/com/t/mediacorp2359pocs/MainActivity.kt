1d0cf2f761f93537fe4e71e47c3bdc8766c4a21d
tructran2359 <truc.trantrung@2359media.com>
2020-08-31 10:01:58 +0700

    [INIT]

---
+++ MediaCorpPOC/app/src/main/java/com/t/mediacorp2359pocs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.t.mediacorp2359pocs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
